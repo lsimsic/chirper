f9ae72941b11f8fb5b88e832fa2da769d0434759
lsimsic <lsimsic@gmail.com>
2026-09-17 16:00:33 -0500

    fix: break long chirp messages onto multiple lines

--- resources/views/components/chirp.blade.php
+++ resources/views/components/chirp.blade.php
@@ -28,7 +28,7 @@
                     <span class="text-sm text-base-content/60">{{ $chirp->created_at->diffForHumans() }}</span>
                 </div>
 
-                <p class="mt-1">
+                <p class="mt-1 break-words">
                     {{ $chirp->message }}
                 </p>
             </div>
