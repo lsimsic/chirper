ac8e26b51c65a698390b18b020a39342e83643fe
Lara Simsic <lsimsic@gmail.com>
2026-09-18 16:23:34 -0500

    feat: login and logout routes, controllers, login blade template (#10)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
@@ -28,3 +30,16 @@ Route::view('/register', 'auth.register')
 
 Route::post('/register', Register::class)
     ->middleware('guest');
+
+// Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
