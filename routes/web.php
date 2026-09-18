d28e17491bf5cddc172d720180a495d3395b31a2
lsimsic <lsimsic@gmail.com>
2026-09-18 10:15:14 -0500

    feat: Add routes and controller methods for editing, updating and deleting chirps.  Add edit and delete buttons and edited indicator to chirp blade component.  Add form for editng chirps.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,15 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
+
 Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+// The above 4 routes are for the Chirp resource, but we can also use a
+// resource route to generate them all at once. Uncomment the line below
+// to use a resource route instead of the individual routes above.
+//
+// Route::resource('chirps', ChirpController::class)
+//  ->only(['store', 'edit', 'update', 'destroy']);
